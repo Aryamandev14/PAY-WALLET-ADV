85df60992f1fdcdce486f760947cf2ae98f5a33f
Aryaman Dev Kumar <devaryaman4@gmail.com>
2025-06-22 05:11:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app=express();
 
 const cors = require('cors');
-const corsOptions = {
-  origin: 'https://pay-wallet-adv12.onrender.com',
-  optionsSuccessStatus: 200 
-};
+// const corsOptions = {
+//   origin: 'https://pay-wallet-adv12.onrender.com',
+//   optionsSuccessStatus: 200 
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://pay-wallet-adv12.onrender.com', // Your frontend URL
+  methods: ['POST', 'GET']
+}));
 app.use(express.json());
 const mainRouter=require("./routes/index");
 app.use("/api/v1",mainRouter)
