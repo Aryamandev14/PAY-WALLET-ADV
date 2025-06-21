132ce8deb44f2129eb59fa2e6b367df383882817
Aryaman Dev Kumar <devaryaman4@gmail.com>
2025-06-22 04:36:50 +0530

    Update SendMoney.jsx

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -20,7 +20,7 @@ export const SendMoney = () => {
         setMessage("");
 
         try {
-            const response = await axios.post("http://localhost:3000/api/v1/account/transfer", {
+            const response = await axios.post("https://pay-wallet-adv-1.onrender.com/api/v1/account/transfer", {
                 to: id,
                 amount
             }, {
